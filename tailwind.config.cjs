98d08cab664f368e868a6412874ffdf48a4fc806
cfx78 <104265674+cfx78@users.noreply.github.com>
2023-09-12 20:29:39 -0500

    changing paths again

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			backgroundImage: {
-				'rmg-logo': 'url(/images/Real_Muziq_Group_Logo_White.png)',
-				t_o: 'url(/images/t_o.png)',
-				teezy: 'url(/images/teezy.png)',
-				smoke: 'url(/images/smoke.png)',
-				studio: 'url(/images/studio.png)',
-				'rmg-gospel': 'url(/images/RMG_Gospel_Logo_White.png)',
+				'rmg-logo': 'url(./src/images/Real_Muziq_Group_Logo_White.png)',
+				t_o: 'url(./src/images/t_o.png)',
+				teezy: 'url(./src/images/teezy.png)',
+				smoke: 'url(./src/images/smoke.png)',
+				studio: 'url(./src/images/studio.png)',
+				'rmg-gospel': 'url(./src/images/RMG_Gospel_Logo_White.png)',
 			},
 		},
 	},
