8cfe3f7618eda14b780402a4754278586168b416
cfx78 <104265674+cfx78@users.noreply.github.com>
2023-09-12 20:32:56 -0500

    testing path

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,8 @@ module.exports = {
 	theme: {
 		extend: {
 			backgroundImage: {
-				'rmg-logo': 'url(./src/images/Real_Muziq_Group_Logo_White.png)',
+				'rmg-logo':
+					'url(../../src/images/Real_Muziq_Group_Logo_White.png)',
 				t_o: 'url(./src/images/t_o.png)',
 				teezy: 'url(./src/images/teezy.png)',
 				smoke: 'url(./src/images/smoke.png)',
